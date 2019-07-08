3920265c8eaf299c970268669cd47ff0191a64ae
Karkalik <Matej.Karkalik@adastragrp.com>
2019-07-08 10:26:56 +0200

    change, python login script, data

--- 01_download_table.R
+++ 01_download_table.R
@@ -1,10 +1,52 @@
 library(rvest)
 library(dplyr)
+library(tidyr)
 
+url <- paste0('https://www.strava.com/running_races/', url, '/results?page=')
 
-url <- "https://www.strava.com/running_races/2428/results?page="
-url_i <- paste0(url, "1")
-
+i <- 1
+url_i <- paste0(url, i)
 page <- read_html(url_i)
-table <- html_table(page, fill = TRUE)
-table_1 <- table[[1]]
+table <- html_table(page, fill = TRUE)[[1]]
+links <- page %>% 
+  html_nodes('.dense.striped') %>% 
+  html_nodes('a') %>% 
+  html_attr('href')
+
+athlet_url <- links[seq(1,length(links),3)]
+race_url <- links[seq(3,length(links),3)]
+
+table['athlet_url'] <- athlet_url
+table['race_url'] <- race_url
+
+table_final <- table 
+
+statment <- T
+
+while (statment)
+{
+  i <- i + 1
+  url_i <- paste0(url, i)
+  page <- read_html(url_i)
+  table <- html_table(page, fill = TRUE)[[1]]
+  
+  statment <- dim(table)[1] != 0
+  
+  if (statment) {
+    links <- page %>%
+      html_nodes('.dense.striped') %>%
+      html_nodes('a') %>%
+      html_attr('href')
+    
+    athlet_url <- links[seq(1, length(links), 3)]
+    race_url <- links[seq(3, length(links), 3)]
+    
+    table['athlet_url'] <- athlet_url
+    table['race_url'] <- race_url
+    
+    table_final <- union(table_final, table)
+  }
+}
+
+write.csv(table_final, paste0(city,"_",type,"_",year,"_raw",".csv"))
+
---
+++ 99_source_chicago.R
@@ -0,0 +1,25 @@
+url <- '2018-chicago-marathon'
+city <- 'chicago'
+year <- '2018'
+type <- 'full'
+
+source('01_download_table.R', echo = TRUE)
+rm(list=ls())
+
+url <- '2017-chicago-marathon'
+city <- 'chicago'
+year <- '2017'
+type <- 'full'
+
+source('01_download_table.R', echo = TRUE)
+rm(list=ls())
+
+url <- '2016-chicago-marathon'
+city <- 'chicago'
+year <- '2016'
+type <- 'full'
+
+source('01_download_table.R', echo = TRUE)
+rm(list=ls())
+
+
